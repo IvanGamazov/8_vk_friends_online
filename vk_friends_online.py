581ed7dd2005438b3dae40a261e150b7a3c008cd
IvanGamazov <ivan.gamazov@gmail.com>
2016-10-18 13:11:58 +0300

    Убран ACCESS_TOKEN, убрана сериализация id пользователей в строку.

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -2,7 +2,6 @@ import vk
 import getpass
 
 APP_ID = 5662270
-ACCESS_TOKEN = 'Mu391hpLUSWD8y0hJorQ'
 
 
 def get_user_login():
@@ -16,7 +15,6 @@ def get_user_password():
 def get_online_friends(usr_login, usr_password):
     session = vk.AuthSession(
         app_id=APP_ID,
-        access_token=ACCESS_TOKEN,
         user_login=usr_login,
         user_password=usr_password,
         scope='friends'
@@ -25,8 +23,7 @@ def get_online_friends(usr_login, usr_password):
     api = vk.API(session)
     ids_of_friends_online = api.friends.getOnline()
     online_friends = api.users.get(
-        user_ids=', '.join([str(friend_id)
-                            for friend_id in ids_of_friends_online])
+        user_ids=[str(friend_id) for friend_id in ids_of_friends_online]
     )
     return online_friends
 
